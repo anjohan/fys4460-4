ae11bfb34cc84de7a9abacfdd3073175c851baf8
Anders Johansson <anjohan@uio.no>
2018-06-11 14:52:07 +0200

    r^2 collapse worked (!)

--- src/r2.f90
+++ src/r2.f90
@@ -7,9 +7,12 @@ program findk
     integer :: L, num_systems, num_steps, num_walkers, num_ps, i, j
     integer :: fileunit
     real(kind=dp), dimension(:,:), allocatable :: displacements
-    real(kind=dp), dimension(:), allocatable :: ps, xis
+    real(kind=dp), dimension(:), allocatable :: ps, xis, Ds, logt, logr2
     integer, dimension(:), allocatable :: t, t0s
-    real(kind=dp) :: k, slope, const, expconst, nu, slope2, const2
+    real(kind=dp) :: k, slope, const, expconst, nu, slope2, const2, tmp_const
+    real(kind=dp) :: mu, Dconst, x
+    real(kind=dp), dimension(:,:), allocatable :: x_collapse, y_collapse
+    real(kind=dp), dimension(:), allocatable :: t2k, tx
     character(len=:), allocatable :: filename
 
     L = 512
@@ -51,6 +54,35 @@ program findk
     call linfit(log(ps(2:) - pc), log(xis), slope, const)
     call linfit(log(ps(2:) - pc), log(1.0d0*t0s), slope2, const2)
     nu = -slope
+
+    allocate(Ds(2:num_ps))
+    do i = 2, num_ps
+        call linfit(1.0d0*t(t0s(i):), displacements(t0s(i):, i), Ds(i), tmp_const)
+    end do
+
+    call linfit(log(ps(2:)-pc), log(Ds), mu, Dconst)
+    logt = log(1.0d0*t(1:))
+    logr2 = log(displacements(1:,1))
+    call linfit(logt, logr2, k, tmp_const)
+    k = k/2
+    x = (1-2*k)/mu
+    t2k = t(1:)**(-2*k)
+    tx = t(1:)**x
+    write(*,*) t2k(:10)
+
+    allocate(x_collapse(num_steps, 2:num_ps))
+    allocate(y_collapse(num_steps, 2:num_ps))
+
+    do i = 2, num_ps
+        x_collapse(:,i) = (ps(i) - pc)*tx
+        y_collapse(:,i) = t2k * displacements(1:,i)
+    end do
+
+    open(newunit=fileunit, file="tmp/r2_collapse.dat", status="replace")
+    do i = 1, num_steps, num_steps/1000
+        write(unit=fileunit, fmt="(*(f0.12,x))") [([x_collapse(i,j),y_collapse(i,j)], &
+                                                  j = 2, num_ps)]
+    end do
 
     open(newunit=fileunit, file="tmp/r2.dat", status="replace")
     do i = 1, num_steps, num_steps/1000
@@ -101,5 +133,15 @@ program findk
 
     open(newunit=fileunit, file="tmp/r2_t0pow.dat", status="replace")
     write(unit=fileunit, fmt="(f0.3)") slope2
+    close(unit=fileunit)
+
+    open(newunit=fileunit, file="tmp/r2_mu.dat", status="replace")
+    write(unit=fileunit, fmt="(f0.3)") mu
+    close(unit=fileunit)
+
+    open(newunit=fileunit, file="tmp/r2_D.dat", status="replace")
+    do i = 2, num_ps
+        write(unit=fileunit, fmt="(f0.3,x,f0.3,x,f0.3)") ps(i)-pc, Ds(i), exp(Dconst)*(ps(i)-pc)**mu
+    end do
     close(unit=fileunit)
 end program findk
